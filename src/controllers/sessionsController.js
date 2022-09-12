28ae90071a7bc82ff5252e584b018f0b2e1ef9df
henriquesdias <henrique.dias1915@gmail.com>
2022-09-12 03:03:32 -0300

    fixing the route '/session'

--- src/controllers/sessionsController.js
+++ src/controllers/sessionsController.js
@@ -1,10 +1,13 @@
 import db from "../database/mongo.js";
 async function deleteSession(req, res) {
-  const token = res.locals.token;
+  const token = res.locals.token.toString();
   try {
     const session = await db.collection("sessions").findOne({ token });
-    console.log(session);
-    res.status(200).send(session);
+    if (!session) {
+      return res.sendStatus(404);
+    }
+    await db.collection("sessions").deleteOne({ token });
+    res.sendStatus(200);
   } catch (error) {
     res.status(500).send(error.message);
   }
