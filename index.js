5cb47cb0672d577c766d890cc3d4c7ab603d9782
henriquesdias <henrique.dias1915@gmail.com>
2022-09-13 15:09:31 -0300

    chore: adjust code for deploy on heroku

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import cors from "cors";
 import userRoutes from "./src/Routes/userRoutes.js";
 import registryRoutes from "./src/Routes/registryRoutes.js";
 import sessionRoutes from "./src/Routes/sessionsRoutes.js";
+import dotenv from "dotenv";
+dotenv.config();
+
 const server = express();
 server.use(express.json());
 server.use(cors());
@@ -10,4 +13,6 @@ server.use(userRoutes);
 server.use(registryRoutes);
 server.use(sessionRoutes);
 
-server.listen(5000, () => console.log("Listening on port 5000"));
+server.listen(process.env.PORT, () =>
+  console.log(`Listening on port ${process.env.PORT}`)
+);
